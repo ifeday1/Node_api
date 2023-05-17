6ec151f276044434f02203a5f32f1497346dc74d
ifeday1 <oladeniifeoluwa123@gmail.com>
2023-05-17 23:33:32 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose');
+
+const app = express()
+
+//Routes
+app.get('/', (req, res) =>{
+    res.send("Node app running")
+})
+
+
+
+mongoose.
+connect("mongodb+srv://oladeniifeoluwa123:olawunmi123@ifeapi.ew9rmp5.mongodb.net/ifeAPI?retryWrites=true&w=majority")
+.then (() => {
+    console.log("CONNECTED")
+    app.listen(3000, ()=>{
+    console.log("The app is running on port 3000")
+})
+})
+.catch((error) =>{
+    console.log(error)
+})
